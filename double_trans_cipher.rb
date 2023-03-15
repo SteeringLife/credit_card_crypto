8ef5284540cf96ff5dcc681009a489ce63a18605
shinstar5 <peiyu.shinstar5@gapp.nthu.edu.tw>
2023-03-15 22:08:55 +0800

    Part B DoubleTranspositionCipher decrypte failed, 5 test pass,robocop fix

--- double_trans_cipher.rb
+++ double_trans_cipher.rb
@@ -1,15 +1,35 @@
+# frozen_string_literal: true
+
+require_relative './credit_card'
+require 'matrix'
+
+# DoubleTranspositionCipher
 module DoubleTranspositionCipher
   def self.encrypt(document, key)
-    # TODO: FILL THIS IN!
-    ## Suggested steps for double transposition cipher
+    document = document.to_s
     # 1. find number of rows/cols such that matrix is almost square
+    size = Math.sqrt(document.length).ceil
+    rows = (document + ' ' * (size**2 - document.length)).chars.each_slice(size).to_a
     # 2. break plaintext into evenly sized blocks
-    # 3. sort rows in predictibly random way using key as seed
-    # 4. sort columns of each row in predictibly random way
+    cols = rows.transpose
+    # 3. sort rows in predictably random way using key as seed
+    rows.shuffle!(random: Random.new(key))
+    # 4. sort columns of each row in predictably random way
+    cols.each { |col| col.shuffle!(random: Random.new(key)) }
     # 5. return joined cyphertext
+    cols.transpose.join
   end
 
   def self.decrypt(ciphertext, key)
-    # TODO: FILL THIS IN!
+    # 1. find number of rows/cols such that matrix is almost square
+    size = Math.sqrt(ciphertext.length).ceil
+    cols = ciphertext.split('\"').reject { |s| s.strip == '' }
+    # 2. sort columns of each row in predictably random way
+    cols.each { |col| col.gsub!(/\s/, '').chars.shuffle!(random: Random.new(key)) }
+    # 3. sort rows in predictibly random way using key as seed
+    rows = cols.each_slice(size).to_a
+    rows.shuffle!(random: Random.new(key))
+    # 4. return joined plaintext
+    rows.transpose.flatten.join.gsub(/\s+$/, '')
   end
 end
--- substitution_cipher.rb
+++ substitution_cipher.rb
@@ -1,4 +1,9 @@
+# frozen_string_literal: true
+
+require_relative './credit_card'
+
 module SubstitutionCipher
+  # Caesar cipher
   module Caesar
     # Encrypts document using key
     # Arguments:
@@ -7,6 +12,8 @@ module SubstitutionCipher
     # Returns: String
     def self.encrypt(document, key)
       # TODO: encrypt string using caesar cipher
+      document = document.to_s
+      document.chars.map { |char| shift_char(char, key) }.join
     end
 
     # Decrypts String document using integer key
@@ -16,9 +23,20 @@ module SubstitutionCipher
     # Returns: String
     def self.decrypt(document, key)
       # TODO: decrypt string using caesar cipher
+      encrypt(document, -key)
+    end
+
+    def self.shift_char(char, key)
+      if char.match?(/[A-Za-z]/)
+        base = char.ord < 97 ? 65 : 97
+        ((char.ord - base + key) % 26 + base).chr
+      else
+        char
+      end
     end
   end
 
+  # Permutation ciphe
   module Permutation
     # Encrypts document using key
     # Arguments:
@@ -27,6 +45,10 @@ module SubstitutionCipher
     # Returns: String
     def self.encrypt(document, key)
       # TODO: encrypt string using a permutation cipher
+      document = document.to_s
+      srand(key)
+      lookup = (0..127).to_a.shuffle(random: Random.new(key))
+      document.chars.map { |char| lookup[char.ord].chr }.join
     end
 
     # Decrypts String document using integer key
@@ -36,6 +58,10 @@ module SubstitutionCipher
     # Returns: String
     def self.decrypt(document, key)
       # TODO: decrypt string using a permutation cipher
+      srand(key)
+      lookup = (0..127).to_a.shuffle(random: Random.new(key))
+      inverse_lookup = lookup.map.with_index.to_h
+      document.chars.map { |char| inverse_lookup[char.ord].chr }.join
     end
   end
 end
