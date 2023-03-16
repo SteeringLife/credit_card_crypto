f704a6dec81712c47bf2e01747ee268994506e02
YKGio <aresr417@gmail.com>
2023-03-16 17:27:18 +0800

    update readability

--- double_trans_cipher.rb
+++ double_trans_cipher.rb
@@ -15,7 +15,7 @@ module DoubleTranspositionCipher
 
   def self.recover(arr, key)
     keys = Array.new(arr.length) { |i| i + 1 }
-    arr.sort_by { |ele_arr| keys.shuffle(random: Random.new(key))[arr.index(ele_arr)] }
+    arr.sort_by { |keys_ele| keys.shuffle(random: Random.new(key))[arr.index(keys_ele)] }
   end
 
   def self.encrypt(document, key)
@@ -36,8 +36,8 @@ module DoubleTranspositionCipher
   def self.decrypt(ciphertext, key)
     # 1. find number of rows/cols such that matrix is almost square
     size = get_size ciphertext
-    # 2. sort columns of each row in predictably random way
     cols = (ciphertext + ' ' * (size**2 - ciphertext.length)).chars.each_slice(size).to_a
+    # 2. sort columns of each row in predictably random way
     cols_original = recover(cols, key)
     # 3. sort rows in predictibly random way using key as seed
     rows = cols_original.transpose
