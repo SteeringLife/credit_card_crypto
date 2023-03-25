7b2f1e67fcf9c29d916806134fe7a76f53ae0079
YKGio <aresr417@gmail.com>
2023-03-25 22:00:30 +0800

    update ModernSymmetricCipher and tests passed

--- sk_cipher.rb
+++ sk_cipher.rb
@@ -1,15 +1,27 @@
+require 'rbnacl'
+require 'base64'
+
 module ModernSymmetricCipher
   def self.generate_new_key
     # TODO: Return a new key as a Base64 string
+    key = RbNaCl::Random.random_bytes(RbNaCl::SecretBox.key_bytes)
+    Base64.strict_encode64(key)
   end
 
   def self.encrypt(document, key)
     # TODO: Return an encrypted string
     #       Use base64 for ciphertext so that it is sendable as text
+    doc_str = document.to_s
+    key_bytes = Base64.strict_decode64(key)
+    ciphertext = RbNaCl::SimpleBox.from_secret_key(key_bytes).encrypt(doc_str)
+    Base64.strict_encode64(ciphertext)
   end
 
   def self.decrypt(encrypted_cc, key)
     # TODO: Decrypt from encrypted message above
     #       Expect Base64 encrypted message and Base64 key
+    key_bytes = Base64.strict_decode64(key.to_s)
+    ciphertext = Base64.strict_decode64(encrypted_cc)
+    RbNaCl::SimpleBox.from_secret_key(key_bytes).decrypt(ciphertext)
   end
 end
